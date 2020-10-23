92a28397de15f03f1b650c5f7ca5f4a2e7130d21
Min <minkimcodes@gmail.com>
2020-10-23 16:25:56 -0400

    express setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.listen(3000, function() {
+    console.log('listening on 3000')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+console.log("May Node be with you")
